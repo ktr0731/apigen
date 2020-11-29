9ffc125306b58b32c9875db47e0e2cb3da6ad2f1
ktr0731 <ktr@syfm.me>
2020-11-30 00:24:14 +0900

    write out to file

--- option.go
+++ option.go
@@ -1,13 +1,23 @@
 package apigen
 
-import "net/http"
+import (
+	"net/http"
+)
 
 // Option represents an option for Generate.
 type Option func(*runner)
 
 // WithHTTPClient specifies the HTTP client for invoking HTTP requests to know API structure.
+// Default is *http.DefaultClient.
 func WithHTTPClient(c *http.Client) Option {
 	return func(r *runner) {
 		r.client = c
+	}
+}
+
+// WithOutputDirectory specifies the destination directory for generated files. Default is the current directory.
+func WithOutputDirectory(d string) Option {
+	return func(r *runner) {
+		r.outDir = d
 	}
 }
--- runner.go
+++ runner.go
@@ -2,9 +2,12 @@ package apigen
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"os"
+	"path/filepath"
 
+	"github.com/iancoleman/strcase"
 	"github.com/morikuni/failure"
 	"golang.org/x/sync/errgroup"
 )
@@ -18,11 +21,13 @@ func Generate(ctx context.Context, def *Definition, opts ...Option) error {
 type runner struct {
 	client  *http.Client
 	decoder decoder
+	outDir  string
 }
 
 var defaultRunner = runner{
 	client:  http.DefaultClient,
 	decoder: &jsonDecoder{},
+	outDir:  ".",
 }
 
 func newRunner(opts ...Option) *runner {
@@ -39,52 +44,70 @@ func (r *runner) run(ctx context.Context, def *Definition) error {
 		return failure.Wrap(err)
 	}
 
-	gen := newGenerator(os.Stdout)
+	if err := os.MkdirAll(r.outDir, 0755); err != nil {
+		return failure.Wrap(err)
+	}
 
 	eg, cctx := errgroup.WithContext(ctx)
-
 	for service, methods := range def.Services {
-		for _, m := range methods {
-			req, err := m.Request(cctx)
+		eg.Go(func() error {
+			return r.processService(cctx, service, methods)
+		})
+	}
+
+	return eg.Wait()
+}
+
+func (r *runner) processService(ctx context.Context, service string, methods []*Method) error {
+	f, err := os.Create(filepath.Join(r.outDir, fmt.Sprintf("%s.go", strcase.ToSnake(service))))
+	if err != nil {
+		return failure.Wrap(err)
+	}
+	defer f.Close()
+
+	eg, cctx := errgroup.WithContext(ctx)
+	gen := newGenerator(f)
+
+	for _, m := range methods {
+		req, err := m.Request(cctx)
+		if err != nil {
+			return failure.Wrap(err)
+		}
+
+		m := m
+
+		eg.Go(func() error {
+			res, err := r.client.Do(req)
+			if err != nil {
+				return failure.Wrap(err)
+			}
+			defer res.Body.Close()
+
+			methRes, err := r.decoder.Decode(res.Body)
 			if err != nil {
 				return failure.Wrap(err)
 			}
 
-			m := m
+			var methReq _type
+			switch req.Method {
+			case http.MethodGet:
+				methReq = structFromQuery(req.URL.Query())
+			default:
+				panic("not implemnted yet")
+			}
 
-			eg.Go(func() error {
-				res, err := r.client.Do(req)
-				if err != nil {
-					return failure.Wrap(err)
-				}
-				defer res.Body.Close()
-
-				methRes, err := r.decoder.Decode(res.Body)
-				if err != nil {
-					return failure.Wrap(err)
-				}
-
-				var methReq _type
-				switch req.Method {
-				case http.MethodGet:
-					methReq = structFromQuery(req.URL.Query())
-				default:
-					panic("not implemnted yet")
-				}
-
-				u := req.URL
-				u.RawQuery = ""
-				gen.addMethod(service+"Client", &method{
-					Name:   m.Name,
-					method: req.Method,
-					url:    u.String(),
-					req:    methReq,
-					res:    methRes,
-				})
-
-				return nil
+			u := req.URL
+			u.RawQuery = ""
+			gen.addMethod(service+"Client", &method{
+				Name:   m.Name,
+				method: req.Method,
+				url:    u.String(),
+				req:    methReq,
+				res:    methRes,
 			})
-		}
+
+			return nil
+		})
 	}
 
 	if err := eg.Wait(); err != nil {
